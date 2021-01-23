6aa1b63d70db0f48d34332367e11f7fa33d99065
Bartlomiej Gladys <bartekgladys@gmail.com>
2021-01-23 01:10:02 +0100

    add after save signal in song

--- myapi/models.py
+++ myapi/models.py
@@ -1,13 +1,23 @@
+from myapi.separate_song import SeparateSongThread
+import os
+from voiceMelody.settings import BASE_DIR
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Song(models.Model):
-  # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, blank=True, null=True, default="No Title")
     song = models.FileField(upload_to="media")
-    voice = models.FileField(upload_to="media")
-    melody = models.FileField(upload_to="media")
+    # voice = models.FileField(upload_to="media", requ)
+    # melody = models.FileField(upload_to="media")
 
     def __str__(self):
         return self.title
+
+
+@receiver(post_save, sender=Song)
+def my_handler(sender, instance: Song, **kwargs):
+    SeparateSongThread(instance).start()
