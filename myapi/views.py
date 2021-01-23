e68df02ef229379506b933da9448eeb3a356d098
Bartlomiej Gladys <bartekgladys@gmail.com>
2021-01-23 01:10:23 +0100

    reformat files & add gitignore

--- myapi/views.py
+++ myapi/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, generics, permissions
 from .serializers import SongSerializer, UserSerializer, RegisterSerializer
-from .models import  Song
+from .models import Song
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from knox.models import AuthToken
@@ -11,7 +12,7 @@ from knox.views import LoginView as KnoxLoginView
 from rest_framework import status
 from django.contrib.auth.models import User
 from .serializers import ChangePasswordSerializer
-from rest_framework.permissions import IsAuthenticated   
+from rest_framework.permissions import IsAuthenticated
 from django.http import Http404
 
 # Register API
@@ -22,26 +23,33 @@ class RegisterAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
-        })
+        return Response(
+            {
+                "user": UserSerializer(
+                    user, context=self.get_serializer_context()
+                ).data,
+                "token": AuthToken.objects.create(user)[1],
+            }
+        )
 
-#Login API
+
+# Login API
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
         login(request, user)
-        return super(LoginAPI, self).post(request, format=None) 
+        return super(LoginAPI, self).post(request, format=None)
+
 
 class ChangePasswordView(generics.UpdateAPIView):
     """
     An endpoint for changing password.
     """
+
     serializer_class = ChangePasswordSerializer
     model = User
     permission_classes = (IsAuthenticated,)
@@ -56,16 +64,24 @@ class ChangePasswordView(generics.UpdateAPIView):
 
         if serializer.is_valid():
             if not self.object.check_password(serializer.data.get("old_password")):
-                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
-            if serializer.data.get("new_password") != serializer.data.get("new_password2"):
-                return Response({"new_password": ["Password fields didn't match."]}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"old_password": ["Wrong password."]},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            if serializer.data.get("new_password") != serializer.data.get(
+                "new_password2"
+            ):
+                return Response(
+                    {"new_password": ["Password fields didn't match."]},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             self.object.set_password(serializer.data.get("new_password"))
             self.object.save()
             response = {
-                'status': 'success',
-                'code': status.HTTP_200_OK,
-                'message': 'Password updated successfully',
-                'data': []
+                "status": "success",
+                "code": status.HTTP_200_OK,
+                "message": "Password updated successfully",
+                "data": [],
             }
 
             return Response(response)
