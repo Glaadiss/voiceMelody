6699652b9e60efe6e81552dce789741e72384188
Anna Szaforz <annaszaforz1@gmail.com>
2021-01-25 10:34:54 +0100

    modified SeparateSongThread

--- myapi/separate_song.py
+++ myapi/separate_song.py
@@ -9,7 +9,6 @@ from django.conf import settings
 class SeparateSongThread(threading.Thread):
     def __init__(self, instance, **kwargs):
         self.instance = instance
-        super(SeparateSongThread, self).__init__(**kwargs)
 
     def run(self):
 
@@ -18,7 +17,6 @@ class SeparateSongThread(threading.Thread):
         separator = Separator("spleeter:2stems")
         MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-        separator.separate_to_file(self.instance.song.path, MEDIA_ROOT)
 
         self.instance.is_separating = False
         self.instance.is_separated = True
@@ -27,7 +25,6 @@ class SeparateSongThread(threading.Thread):
         self.instance.melody.name = f"{song_base}/accompaniment.wav"
         self.instance.save()
         subject = f"Piosenka {self.instance.title} gotowa!"
-        message = "Hello world"
         email_from = settings.EMAIL_HOST_USER
         recipient_list = [self.instance.user.email]
-        send_mail(subject, message, email_from, recipient_list)
+
--- myapi/views.py
+++ myapi/views.py
@@ -1,7 +1,7 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import serializers, viewsets, generics, permissions
+from rest_framework import serializers, viewsets, generics, permissions, status
 from .serializers import SongSerializer, UserSerializer, RegisterSerializer
 from .models import Song
 from rest_framework.decorators import api_view
@@ -13,85 +13,10 @@ from django.contrib.auth import login
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 
 # from knox.views import LoginView as KnoxLoginView
-from rest_framework import status
 from django.contrib.auth.models import User
 from .serializers import ChangePasswordSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.http import Http404
-
-# Register API
-# class RegisterAPI(generics.GenericAPIView):
-#     serializer_class = RegisterSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.save()
-#         return Response(
-#             {
-#                 "user": UserSerializer(
-#                     user, context=self.get_serializer_context()
-#                 ).data,
-#                 "token": AuthToken.objects.create(user)[1],
-#             }
-#         )
-
-
-# # Login API
-# class LoginAPI(KnoxLoginView):
-#     permission_classes = (permissions.AllowAny,)
-
-#     def post(self, request, format=None):
-#         serializer = AuthTokenSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.validated_data["user"]
-#         login(request, user)
-#         return super(LoginAPI, self).post(request, format=None)
-
-
-# class ChangePasswordView(generics.UpdateAPIView):
-#     """
-#     An endpoint for changing password.
-#     """
-
-#     serializer_class = ChangePasswordSerializer
-#     model = User
-#     permission_classes = (IsAuthenticated,)
-
-#     def get_object(self, queryset=None):
-#         obj = self.request.user
-#         return obj
-
-#     def update(self, request, *args, **kwargs):
-#         self.object = self.get_object()
-#         serializer = self.get_serializer(data=request.data)
-
-#         if serializer.is_valid():
-#             if not self.object.check_password(serializer.data.get("old_password")):
-#                 return Response(
-#                     {"old_password": ["Wrong password."]},
-#                     status=status.HTTP_400_BAD_REQUEST,
-#                 )
-#             if serializer.data.get("new_password") != serializer.data.get(
-#                 "new_password2"
-#             ):
-#                 return Response(
-#                     {"new_password": ["Password fields didn't match."]},
-#                     status=status.HTTP_400_BAD_REQUEST,
-#                 )
-#             self.object.set_password(serializer.data.get("new_password"))
-#             self.object.save()
-#             response = {
-#                 "status": "success",
-#                 "code": status.HTTP_200_OK,
-#                 "message": "Password updated successfully",
-#                 "data": [],
-#             }
-
-#             return Response(response)
-
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
