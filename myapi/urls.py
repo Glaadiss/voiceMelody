df5fae3a820cf51139347137e82da6447b728385
Anna Szaforz <annaszaforz1@gmail.com>
2021-01-25 10:56:47 +0100

    reformat files

--- myapi/urls.py
+++ myapi/urls.py
@@ -11,6 +11,7 @@ router.register(r"song", views.SongViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path("", include(router.urls)),
 
--- myapi/views.py
+++ myapi/views.py
@@ -42,3 +42,84 @@ class SongViewSet(viewsets.ModelViewSet):
             content_type="text/json-comment-filtered",
         )
         # return Response(serializer.data)
+
+
+
+# Register API
+
+# class RegisterAPI(generics.GenericAPIView):
+#     serializer_class = RegisterSerializer
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.save()
+#         return Response(
+#             {
+#                 "user": UserSerializer(
+#                     user, context=self.get_serializer_context()
+#                 ).data,
+#                 "token": AuthToken.objects.create(user)[1],
+#             }
+#         )
+
+
+# # Login API
+
+# class LoginAPI(KnoxLoginView):
+#     permission_classes = (permissions.AllowAny,)
+
+#     def post(self, request, format=None):
+#         serializer = AuthTokenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data["user"]
+#         login(request, user)
+#         return super(LoginAPI, self).post(request, format=None)
+
+
+# class ChangePasswordView(generics.UpdateAPIView):
+
+#     """
+#     An endpoint for changing password.
+#     """
+
+#     serializer_class = ChangePasswordSerializer
+#     model = User
+#     permission_classes = (IsAuthenticated,)
+
+#     def get_object(self, queryset=None):
+#         obj = self.request.user
+#         return obj
+
+#     def update(self, request, *args, **kwargs):
+#         self.object = self.get_object()
+#         serializer = self.get_serializer(data=request.data)
+
+#         if serializer.is_valid():
+
+#             if not self.object.check_password(serializer.data.get("old_password")):
+#                 return Response(
+#                     {"old_password": ["Wrong password."]},
+#                     status=status.HTTP_400_BAD_REQUEST,
+#                 )
+#             if serializer.data.get("new_password") != serializer.data.get(
+#                 "new_password2"
+#             ):
+#                 return Response(
+#                     {"new_password": ["Password fields didn't match."]},
+#                     status=status.HTTP_400_BAD_REQUEST,
+#                 )
+
+#             self.object.set_password(serializer.data.get("new_password"))
+#             self.object.save()
+#             response = {
+#                 "status": "success",
+#                 "code": status.HTTP_200_OK,
+#                 "message": "Password updated successfully",
+#                 "data": [],
+#             }
+
+#             return Response(response)
+
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
